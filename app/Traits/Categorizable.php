c1996a415f2786a16ff710a8b663e2749c69ad1e
Marley Rae <marleyrae1997@gmail.com>
2022-04-23 19:53:35 -0700

    no

--- app/Traits/Categorizable.php
+++ app/Traits/Categorizable.php
@@ -6,8 +6,26 @@ use App\Models\Category;
 
 trait Categorizable
 {
+/* ---------------------------------------------------------------------------- relationship ---- */
+
 	public function categories()
 	{
 		return $this->morphToMany(Category::class, 'categorizable');
+	}
+
+/* -------------------------------------------------------------------------- listCategories ---- */
+
+	public function listCategories() : string
+	{
+		$cats = $this->categories;
+		$str  = '';
+
+		if ($cats->isNotEmpty()) {
+			foreach ($cats as $cat) {
+				$str .= "$cat->name, ";
+			}
+		}
+
+		return rtrim($str, ', ');
 	}
 }
--- resources/views/admin/joined/index.blade.php
+++ resources/views/admin/joined/index.blade.php
@@ -7,6 +7,8 @@
 @section('pg-title', 'Joined')
 
 @section('content')
+@inject('Category', 'App\Models\Category')
+
 	<table class="table">
 		<thead class="table__thead">
 			<tr>
@@ -21,10 +23,17 @@
 		<tbody class="table__tbody">
 			@foreach ($joined as $fl)
 				<tr>
-					<td></td>
+					<td>
+						@if (!$fl->approved)
+							<span class="text--error">&times;</span>
+						@endif
+					</td>
 					<td><a href="{{ $fl->url }}" target="_blank">{{ $fl->subject }}</a></td>
-					<td></td>
+					<td>{{ $fl->listCategories() }}</td>
 					<td><img src="{{ $fl->image }}"></td>
+					<td></td>
+					<td></td>
+					<td></td>
 				</tr>
 			@endforeach
 		</tbody>
